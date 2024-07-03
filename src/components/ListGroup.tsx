980b8287d7f5650b499965b3affc01c78d74e644
poulin29 <danielpoulina29@gmail.com>
2024-07-03 16:08:20 -0500

    Handling Click event

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { MouseEvent } from "react";
 
 function ListGroup() {
   let items = ["NewYork", "Chicago", "Arkansas", "Minnesota", "Tennesse"];
-  items = [];
+
+//event Handler
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
@@ -10,8 +13,10 @@ function ListGroup() {
       {/* {items.length === 0 ? <p>No Items found</p> : null}  //In order handle or avoid 'null' scenario I can use logical AND(&&) as written in below line# 11  */}
       {items.length === 0 && <p>No Items found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
