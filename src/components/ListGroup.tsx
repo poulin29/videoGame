d25c9b357661a6f11997389a624f6f2520b0903a
poulin29 <danielpoulina29@gmail.com>
2024-07-03 15:50:34 -0500

    Renderring the list conditionally using the ternary opertaor

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 
 function ListGroup() {
-  const items = ["NewYork", "Chicago", "Arkansas", "Minnesota", "Tennesse"];
+  let items = ["NewYork", "Chicago", "Arkansas", "Minnesota", "Tennesse"];
+  items = [];
 
   return (
     <>
       <h1>ListGroup</h1>
+      {/* {items.length === 0 ? <p>No Items found</p> : null}  //In order handle or avoid 'null' scenario I can use logical AND(&&) as written in below line# 11  */}
+      {items.length === 0 && <p>No Items found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
